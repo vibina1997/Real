0abeee9f0ef6a79b3622f01ae8bf71fdb4f6516e
vibina1997 <vibina178@gmail.com>
2026-01-03 14:53:12 +0530

    smallchanges

--- my-react-app/src/Components/Admin/Topbar.jsx
+++ my-react-app/src/Components/Admin/Topbar.jsx
@@ -3,7 +3,9 @@ import tob from '../../assets/topbar.module.css';
 const Topbar = () => (
   <header className={tob.topbar}>
     <input placeholder="Search properties..." />
+    <div>
     <div className={tob.user}>John Doe</div>
+    </div>
   </header>
 );
 
